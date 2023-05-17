6c2641ed3ed9614850c41ed47bbccc9cc6a2fd57
Srini <srinivasboini7@gmail.com>
2023-05-17 15:35:43 +0800

    Final working changes

--- src/main/java/com/srini/bulkhead/Resilience4jBulkheadThreadpoolApplication.java
+++ src/main/java/com/srini/bulkhead/Resilience4jBulkheadThreadpoolApplication.java
@@ -1,11 +1,27 @@
 package com.srini.bulkhead;
 
+import com.srini.bulkhead.service.AppService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class Resilience4jBulkheadThreadpoolApplication {
 
+	private final AppService appService ;
+
+
+	@EventListener(ApplicationStartedEvent.class)
+	public void execute(){
+		IntStream.rangeClosed(1,20)
+				.parallel()
+				.forEach(i -> appService.get());
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Resilience4jBulkheadThreadpoolApplication.class, args);
 	}
---
+++ src/main/java/com/srini/bulkhead/service/AppService.java
@@ -0,0 +1,33 @@
+package com.srini.bulkhead.service;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+@Slf4j
+public class AppService {
+
+    private final AtomicInteger counter = new AtomicInteger() ;
+
+    @Bulkhead(name = "serviceA", type = Bulkhead.Type.THREADPOOL, fallbackMethod = "fallbackA")
+    public CompletableFuture<String> get(){
+        log.info("get method called {} time(s)", counter.incrementAndGet());
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return  CompletableFuture.completedFuture("success ") ;
+    }
+
+    private CompletableFuture<String> fallbackA(Throwable throwable){
+        log.error("Exception recorded {}", throwable);
+        log.info("fallback called ") ;
+        return CompletableFuture.completedFuture("fallback") ;
+    }
+}
